c6865ff0952251c35c605f25e30823cfb20ec0ff
Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>
2023-02-17 14:49:45 +0000

    add recursive parsing

--- src/main/java/com/ajmaltestorg/testjavaservice/TestJavaServiceApplication.java
+++ src/main/java/com/ajmaltestorg/testjavaservice/TestJavaServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestJavaServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World with not-so-perfect sbom workflow!");
+		System.out.println("Hello World with not-so-perfect sbom  with recursive workflow!");
 		SpringApplication.run(TestJavaServiceApplication.class, args);
 	}
 
